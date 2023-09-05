20ed52313fe07f4458c0e5a6f8f4e00d8468bb89
Artem Moroz <deusestmaleficus@gmail.com>
2023-09-06 00:55:29 +0700

    fixture testing

---
+++ tests/test_append_array.py
@@ -0,0 +1,9 @@
+from src.aapend_array import append_array
+
+
+def test_append_array(array_fixture):
+    assert append_array(array_fixture, 4) == [1, 2, 3, 4]
+
+
+def test_append_array_2(array_fixture):
+    assert append_array(array_fixture, 5) == [1, 2, 3, 5]
